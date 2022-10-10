ec69c4afb7a71ce2bd9fcb0efb2aefa047b54165
Michael Nyamande <nyamandemike43@gmail.com>
2022-10-10 21:38:29 +0200

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.10"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("it.skrape:skrapeit:1.2.2")
+    implementation("org.seleniumhq.selenium:selenium-java:4.5.0")
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
---
+++ src/main/kotlin/DynamicScraping.kt
@@ -0,0 +1,42 @@
+import org.openqa.selenium.By
+import org.openqa.selenium.Keys
+import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.support.ui.ExpectedConditions
+import org.openqa.selenium.support.ui.WebDriverWait
+import java.lang.Thread.sleep
+import java.time.Duration
+
+
+fun main(){
+ System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+
+ val driver = ChromeDriver()
+ driver.get("https://twitter.com/search?q=web3&src=typed_query")
+
+ WebDriverWait(driver, Duration.ofSeconds(10)).until(
+  ExpectedConditions.presenceOfElementLocated(
+   By.cssSelector(
+    "main section"
+   )
+  )
+ )
+
+ val page = driver.findElement(By.cssSelector("body"))
+
+ val links: MutableList<String> = mutableListOf()
+ while(links.size<5){
+  val l = driver.findElements(By.xpath("//div[@data-testid=\"cellInnerDiv\"]//time/.."))
+  for (e in l) {
+   if (e.getAttribute("href")!=null) links.add(e.getAttribute("href"))
+  }
+
+  page.sendKeys(Keys.PAGE_DOWN)
+
+  sleep(1500)
+ }
+
+ driver.quit();
+
+ println(links);
+
+ }
